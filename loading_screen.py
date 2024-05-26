c39e63ad74a5f35e569b6bcd36805b2cf1030c36
BinhDo908 <ducbinhdo908@gmail.com>
2024-05-26 10:18:52 +0700

    Update loading_screen.py

--- loading_screen.py
+++ loading_screen.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from Main import MainWindow  # Import the main window class from main_window.py
+from Main import MainWindow  
 import time
 
 class LoadingScreen:
@@ -8,29 +8,19 @@ class LoadingScreen:
         self.root = tk.Tk()
         self.root.geometry("400x400")
         self.root.configure(bg="#1E1E1E")
-
-        # Center the window on the screen
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         x_coordinate = (screen_width - 500) // 2
         y_coordinate = (screen_height - 500) // 2
         self.root.geometry(f"500x500+{x_coordinate}+{y_coordinate}")
-
-        # Remove the title bar
         self.root.overrideredirect(True)
-
-        # Loading image
         self.image_path = r"C:\Users\Admin\Downloads\Tkinter-Designer-master\Tkinter-Designer-master1\build\Screenshot_2024-05-08_112229-removebg-preview.png"
         self.image = tk.PhotoImage(file=self.image_path)
         self.image = self.image.subsample(2, 2) 
         self.image_label = tk.Label(self.root, image=self.image, bg="#1E1E1E")
         self.image_label.place(relx=0.5, rely=0.25, anchor="center")
-
-        # Loading animation
         self.loading_label = tk.Label(self.root, text="Application Organizer Program", font=("Helvetica", 15, "bold"), fg="#9f0be3", bg="#1E1E1E")
         self.loading_label.place(relx=0.5, rely=0.5, anchor="center")
-
-        # Loading bar
         self.loading_bar = ttk.Progressbar(self.root, orient="horizontal", mode="indeterminate", length=300)
         self.loading_bar.place(relx=0.5, rely=0.7, anchor="center")
         self.loading_bar.start(10)
@@ -51,9 +41,9 @@ class LoadingScreen:
 
 
     def open_main_window(self):
-        self.root.destroy()  # Close the loading screen window
-        main_window = MainWindow()  # Create an instance of the main window
-        main_window.show()  # Show the main window
+        self.root.destroy()  
+        main_window = MainWindow() 
+        main_window.show() 
 
 
 def main():
